9bcc8a5b733cad4f255ed526014137576a9bb50b
0x4A42 <59893695+0x4A42@users.noreply.github.com>
2021-04-27 17:22:56 +0100

    Initial file creation
    Initial creation of files and a skeleton UI constructed.

---
+++ main.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+
+
+app = Flask(__name__)
+app.config
+
+
+@app.route('/', methods=['POST', 'GET'])
+def return_index():
+    """
+    Serves the home page to the user to allow them to shorten a URL.
+    """
+    if request.method == 'GET':
+        return render_template('home.html')
+
+
+@app.route('/results', methods=['POST', 'GET'])
+def return_result():
+    """
+    Processes the user accessing the results page. 
+    If a GET, normal access (not POST from form) and redirects the user to the home page.
+    Else, generates a shortened URL and displays it to the user.
+    """
+    if request.method == 'GET':
+        return redirect('/', code=301)
+    else:
+        #user_input = request.form['urlName']
+        # shortened_url =
+        return render_template('results.html')
+        # return render_template('results.html', url_result=shortened_url)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
---
+++ url_processor.py
@@ -0,0 +1,5 @@
+    """
+    This script will be responsible for the shortening of the URL passed in by the user.
+    """
+    
+    
